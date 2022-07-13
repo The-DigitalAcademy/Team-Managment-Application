633d1cabeb62402a0ccd137489a19093b47695ff
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-13 09:28:57 +0200

    added team

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-const input = document.querySelector("#add");
-const  between = document.querySelector("#btn");
-const list = document.querySelector("#list");
-var element = document.getElementsByTagName('li');
-
-
-btn.onclick = function(){
-    
-    var text = input.value;
-    if(text ==''){
-        alert('you write something');
-    }else{
-        li = document.createElement('li');
-    li.innerHTML = text;
-    list.insertBefore(li,list.childNodes[0]);
-    input.value = ''
+function submitProjectDetails() {
+    var ProjectName = document.getElementById("PName")
+    var pAssigned = document.getElementById("projectAssigned")
+    var sDate = document.getElementById("SDate")
+    var eDate = document.getElementById("EDate")
+    var Prior = document.querySelector('input[name="priority"]')
+    var Description = document.getElementById("description")
+ // project name validation
+    if (ProjectName.value == "") {
+        alert("Project name field cannot be empty");
+    } else {
+        alert(ProjectName.value);
     }
-    
-}
+    // Assigned to validation
+    var person = pAssigned.options[pAssigned.selectedIndex].value;
+    if (person == "" || person=="Select") {
+        alert("Project Assigned to field cannot be empty");
+    } else {
+        alert(pAssigned.options[pAssigned.selectedIndex].value);
+    }
+  // Date validation
+    // start date
+    if (sDate.value == "") {
+        alert(" Please Enter the Starting date");
+    } else {
+        alert(document.getElementById("SDate").value);
+    }
+    // End date
+    if (eDate.value == 0) {
+        alert("Please enter the end date ");
+    } else {
+        alert(document.getElementById("EDate").value);
+    }
+   //priority validation
+    var Prior = document.querySelector('input[name="priority"]:checked');
 
+    if (Prior != null) {
+        alert("The priority of project is:" + Prior.value);
 
-list.onclick = function(alert){
-    if(ev.target.tagName == 'LI'){
-         ev.target.classList.toggle('checked');
+    } else {
+        alert("You did not select the priority of the project");
+    }
+// description validation
+
+    if (Description.value == "") {
+        alert("You did not write the project description");
+    } else {
+        alert(document.getElementById("description").value);
     }
 }
