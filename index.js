4d644341aa1d6ac095619b9693e2213869af0e68
Samkelisiwe <noziphondlovu910@gmail.com>
2022-08-22 09:25:29 +0200

    added var names

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var name;
-var surname;
-var team;
+var names=["Hardy" ,"Dylan" ,"Melisha"];
 
+document.writ(names[0]);
+document.write("<br>");
+
+document.write(names[1]);
+document.write("<br>");
+
+document.write(names[2]);
+document.write("<br>");
